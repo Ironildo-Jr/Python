a23ba9e9fb63bcea0268a64f98ee7f76f4e59ab9
Ironildo Junior <96444550+Ironildo-Jr@users.noreply.github.com>
2024-11-27 14:42:38 -0300

    alterando banco de dados de novo

--- DataBase.py
+++ DataBase.py
@@ -1,13 +1,17 @@
-import psycopg2
+import sqlite3;
 
-conn = psycopg2.connect(host="localhost",database="conceito",user="postgres",password="root")
+conn = sqlite3.connect(":memory:")
 cur = conn.cursor()
 
+cur.execute("CREATE TABLE Fornecedores (id_forn INTEGER PRIMARY KEY,nome TEXT)")
+cur.execute("CREATE TABLE Produtos (id_prod INTEGER PRIMARY KEY,descricao TEXT, preco REAL)")
+cur.execute("CREATE TABLE Cotacao (id_cot INTEGER PRIMARY KEY,data TEXT,hora TEXT,id_forn INTEGER,qtd INTEGER,id_prod INTEGER,total REAL, FOREIGN KEY (id_forn) REFERENCES Fornecedores (id_forn), FOREIGN KEY (id_prod) REFERENCES Produtos (id_prod))")
+conn.commit()
 
 def inserir_forn(nome):
 	cur.execute("""
-		insert into Fornecedores(nome) values(%s);
-	""", (nome,))
+		insert into Fornecedores(nome) values(?);
+	""", (nome))
 	conn.commit()
 
 
@@ -21,21 +25,21 @@ def fornecedores():
 
 def editar_forn(id, nome):
 	cur.execute("""
-		update Fornecedores set nome = %s where id_forn = %s;
+		update Fornecedores set nome = ? where id_forn = ?;
 	""", (nome, id))
 	conn.commit()
 
 
 def excluir_forn(id):
 	cur.execute("""
-		delete from Fornecedores where id_forn = %s
+		delete from Fornecedores where id_forn = ?
 	""", (id,))
 	conn.commit()
 
 
 def inserir_prod(descricao,preco):
 	cur.execute("""
-		insert into Produtos(descricao,preco) values(%s,%s);
+		insert into Produtos(descricao,preco) values(?,?);
 	""", (descricao, preco))
 	conn.commit()
 
@@ -50,28 +54,28 @@ def Produtos():
 
 def editar_prod(id, descricao, preco):
 	cur.execute("""
-		update Produtos set descricao = %s, preco = %s where id_prod = %s;
+		update Produtos set descricao = ?, preco = ? where id_prod = ?;
 	""", (descricao, preco, id))
 	conn.commit()
 
 
 def excluir_prod(id):
 	cur.execute("""
-		delete from Produtos where id_prod = %s
+		delete from Produtos where id_prod = ?
 	""",(id,))
 	conn.commit()
 
 
 def inserir_cot(data, hora, idforn, qtd, idprod, total):
 	cur.execute("""
-		insert into Cotacao(datta,hora,id_forn,qtd,id_prod,total) values(%s,%s,%s,%s,%s,%s);
+		insert into Cotacao(data,hora,id_forn,qtd,id_prod,total) values(?,?,?,?,?,?);
 	""",(data,hora,idforn,qtd,idprod,total))
 	conn.commit()
 
 
 def Cotacao():
 	cur.execute("""
-		select id_cot,datta,hora,Fornecedores.nome,qtd,Produtos.preco,total from Cotacao
+		select id_cot,data,hora,Fornecedores.nome,qtd,Produtos.preco,total from Cotacao
 			inner join Fornecedores
 				on Fornecedores.id_forn=Cotacao.id_forn
 			inner join Produtos
@@ -83,21 +87,21 @@ def Cotacao():
 
 def editar_cot(id, data, hora, idforn, qtd, idprod, total):
 	cur.execute("""
-		update Cotacao set datta = %s, hora = %s, id_forn = %s, qtd = %s, id_prod = %s, total = %s where id_cot = %s;
+		update Cotacao set data = ?, hora = ?, id_forn = ?, qtd = ?, id_prod = ?, total = ? where id_cot = ?;
 	""", (data, hora, idforn, qtd, idprod, total, id))
 	conn.commit()
 
 
 def excluir_cot(id):
 	cur.execute("""
-		delete from Cotacao where id_cot = %s
+		delete from Cotacao where id_cot = ?
 	""",(id,))
 	conn.commit()
 
 
 def preco_prod(idprod):
 	cur.execute("""
-		select preco from Produtos where id_prod = %s
+		select preco from Produtos where id_prod = ?
 	""", (idprod,))
 	for row in cur.fetchall():
 		return row[0]
